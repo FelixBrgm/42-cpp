8d65d68d26ff2f2db0e7d8c3d287d18eaf555af1
fbruggem <fbruggem@student.42heilbronn.de>
2022-10-11 16:35:16 +0200

    upi

---
+++ 07/ex00/whatever.hpp
@@ -0,0 +1,27 @@
+#ifndef WHATEVER_H
+#define WHATEVER_H
+
+template <typename T>
+T max(T x, T y) {
+	if (x > y)
+		return (x);
+	else
+		return (y);
+}
+template <typename T>
+T min(T x, T y) {
+	if (x < y)
+		return (x);
+	else
+		return (y);
+}
+template <typename T>
+void swap(T &x, T &y)
+{
+	T temp;
+
+	temp = x;
+	x = y;
+	y = temp;
+}
+#endif
