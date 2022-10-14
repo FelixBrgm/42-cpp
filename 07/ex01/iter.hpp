cf6876ea66d9270c9838936fd707dd61ba368aec
fbruggem <fbruggem@student.42heilbronn.de>
2022-10-14 17:24:19 +0200

    udpats

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -1,12 +1,12 @@
 #ifndef ITER_H
 #define ITER_H
 
-template<class T>void iter(T *p, int n, void(f)(T t))
+template<class T>void iter(T *p, int n, void(f)(const &T t))
 {
 	if(!p || n < 0)
 		return ;
 	
-	for (int i = 0; i < n && p[i]; i++)
+	for (int i = 0; i < n; i++)
 		f(p[i]);
 }
 
